911bc13b97cab3987af96c76f988b1075042710a
Valeria21A <briali943@gmail.com>
2018-10-27 16:53:40 -0500

    pruebacorreo

--- correo.php
+++ correo.php
@@ -1,33 +1,67 @@
 <?php
-
+//Comprobamos que se haya presionado el boton enviar
+if(isset($_POST[‘enviar’])){
+//Guardamos en variables los datos enviados
 $nombre = $_POST[‘nombre’];
+$email = $_POST[’email’];
+$telefono = $_POST[‘telefono’];
+$ciudad = $_POST[‘ciudad’];
+$pais = $_POST[‘pais’];
+$mensaje = $_POST[‘mensaje’];
 
-$mail = $_POST[‘correo’];
+///Validamos del lado del servidor que el nombre y el email no estén vacios
+if($nombre == ”){
+echo "Debe ingresar su nombre";
+}
+else if($email == ”){
+echo "Debe ingresar su email";
+}else{
+$para = "vale79211@gmail.com";//Email al que se enviará
+$asunto = "Contacto para su sitio web";//Puedes cambiar el asunto del mensaje desde aqui
+//Este sería el cuerpo del mensaje
+$mensaje = "
+<table border=’0′ cellspacing=’3′ cellpadding=’2′>
+<tr>
+<td width=’30%’ align=’left’ bgcolor=’#f0efef’><strong>Nombre:</strong></td>
+<td width=’80%’ align=’left’>$nombre</td>
+</tr>
+<tr>
+<td align=’left’ bgcolor=’#f0efef’><strong>E-mail:</strong></td>
+<td align=’left’>$email</td>
+</tr>
+<tr>
+<td width=’30%’ align=’left’ bgcolor=’#f0efef’><strong>Teléfono:</strong></td>
+<td width=’70%’ align=’left’>$telefono</td>
+</tr>
+<tr>
+<td width=’30%’ align=’left’ bgcolor=’#f0efef’><strong>Ciudad:</strong></td>
+<td width=’70%’ align=’left’>$ciudad</td>
+</tr>
+<tr>
+<td width=’30%’ align=’left’ bgcolor=’#f0efef’><strong>País:</strong></td>
+<td width=’70%’ align=’left’>$pais</td>
+</tr>
+<tr>
+<td align=’left’ bgcolor=’#f0efef’><strong>Comentario:</strong></td>
+<td align=’left’>$mensaje</td>
+</tr>
+</table>
+";
 
-$asunto = $_POST[‘asunto’];
+//Cabeceras del correo
+$headers = "From: $nombre <$email>\r\n"; //Quien envia?
+$headers .= "X-Mailer: PHP5\n";
+$headers .= ‘MIME-Version: 1.0’ . "\n";
+$headers .= ‘Content-type: text/html; charset=iso-8859-1’ . "\r\n"; //
 
-$header = ‘From: ’ . $mail . “ \r\n”;
-
-$header .= “X-Mailer: PHP/” . phpversion() . “ \r\n”;
-
-$header .= “Mime-Version: 1.0 \r\n”;
-
-$header .= “Content-Type: text/plain”;
-
-$mensaje = “Este mensaje fue enviado por ” . $nombre . “ ” . $asunto . “ \r\n”;
-
-$mensaje .= “Su e-mail es: ” . $mail . “ \r\n”;
-
-$mensaje .= “Mensaje: ” . $_POST[‘asunto’] . “ \r\n”;
-
-$mensaje .= “Enviado el ” . date(’d/m/Y’, time());
-
-$para = ‘vale79211@gmail.com’;
-
-$titulo = ‘Mensaje del sitio Web Clover4’;
-
-mail($para, $titulo, utf8_decode($mensaje), $header);
-
-echo “Tus datos fueron enviados correctamente <b>”.$_POST[‘nombre’].“</b>”;
-
+//Comprobamos que los datos enviados a la función MAIL de PHP estén bien y si es correcto enviamos
+if(mail($para, $asunto, $mensaje, $headers)){
+echo "Su mensaje se ha enviado correctamente";
+echo "<br />";
+echo ‘<a href="../formulario_contacto.html">Volver</a>’;
+}else{
+echo "Hubo un error en el envío inténtelo más tarde";
+}
+}
+}
 ?>
