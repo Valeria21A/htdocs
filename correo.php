137e43d4cd48254ce21126b9d74439270c3f1314
Valeria21A <briali943@gmail.com>
2018-10-27 23:38:46 -0500

    pruebacorreo7

--- correo.php
+++ correo.php
@@ -65,7 +65,7 @@ $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1 \r\n"; 
 */
 $headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+$headers .= "Content-type: text/html; charset=utf-8". "\r\n";
 $headers .= "From: $email" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion();
 
 
